578bb37dee0d6a4b9fa95ede24215af90d900dc2
Michal Krupinski <michal.krupinski@fuerogames.pl>
2018-03-22 19:29:57 +0100

    315

---
+++ ComparableCar/Car.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparableCar
+{
+    class Car : IComparable
+    {
+        // Constant for maximum speed.
+        public const int MaxSpeed = 100;
+
+        // Car properties.
+        public int CurrentSpeed { get; set; }
+
+        public string PetName { get; set; }
+
+        public int CarID { get; set; }
+
+        // Is the car still operational?
+        private bool carIsDead;
+
+        // A car has-a radio.
+        private Radio theMusicBox = new Radio();
+
+        // Constructors.
+        public Car()
+        {
+        }
+
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public Car(string name, int currSp, int id)
+        {
+            CurrentSpeed = currSp;
+            PetName = name;
+            CarID = id;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            // Delegate request to inner object.
+            theMusicBox.TurnOn(state);
+        }
+
+        // IComparable implementation
+        int IComparable.CompareTo(object obj)
+        {
+            Car temp = obj as Car;
+            if (temp != null)
+            {
+                if (this.CarID > temp.CarID)
+                    return 1;
+                if (this.CarID < temp.CarID)
+                    return -1;
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+                throw new ArgumentException("Parameter is not a Car!");
+        }
+
+        // See if Car has overheated.
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    carIsDead = true;
+                    CurrentSpeed = 0;
+
+                    // We need to call the HelpLink property, thus we need to
+                    // create a local variable before throwing the Exception object.
+                    Exception ex = new Exception(string.Format("{0} has overheated!", PetName));
+                    ex.HelpLink = "http://www.CarsRUs.com";
+
+                    ex.Data.Add("TimeStamp", string.Format("The car exploded at {0}", DateTime.Now));
+                    ex.Data.Add("Cause", "You have a lead foot.");
+
+
+                    throw ex;
+                    
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
--- ComparableCar/Program.cs
+++ ComparableCar/Program.cs
@@ -10,6 +10,17 @@ namespace ComparableCar
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Object Sorting *****\n");
+            // Make an array of Car objects.
+            Car[] myAutos = new Car[5];
+            myAutos[0] = new Car("Rusty", 80, 1);
+            myAutos[1] = new Car("Mary", 40, 234);
+            myAutos[2] = new Car("Viper", 40, 34);
+            myAutos[3] = new Car("Mel", 40, 4);
+            myAutos[4] = new Car("Chucky", 40, 5);
+            Console.ReadLine();
+
+
         }
     }
 }
